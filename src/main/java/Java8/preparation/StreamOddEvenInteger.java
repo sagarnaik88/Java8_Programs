a2cd11406208e6bd048e8879e3b61a8e93792fdf
Sagar Naik <sagar20688@gmail.com>
2021-10-27 22:28:33 +0530

    [SANA] Simple Java Maven Project

---
+++ src/main/java/Java8/preparation/StreamOddEvenInteger.java
@@ -0,0 +1,21 @@
+package Java8.preparation;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Sagar_27_10_2021
+ *Given a list of integers, find out all the even numbers exist in the list
+ *using Stream functions?
+ */
+public class StreamOddEvenInteger {
+
+	public static void main(String[] args) {
+		List<Integer> intList = Arrays.asList(10,15,8,49,25,98,32);
+		intList.stream()
+			.filter(n -> n%2 == 0)
+			.forEach(System.out::println);
+
+	}
+
+}
