901c4080fb90ebdb77e32042c31b6179b782cb84
Sagar Naik <sagar20688@gmail.com>
2021-11-10 11:05:33 +0530

    [SANA] Find all duplicate values in list using stream API.

--- src/main/java/Java8/stream/api/preparation/DuplicateNumberInList.java
+++ src/main/java/Java8/stream/api/preparation/DuplicateNumberInList.java
@@ -7,6 +7,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * @author Sagar_09_11_2021
+ * Given a list of integers, find out all the Duplicate Number in the list
+ * using Stream functions?
+ */
 public class DuplicateNumberInList {
 
 	public static void main(String[] args) {
@@ -14,5 +19,6 @@ public class DuplicateNumberInList {
 		List<Integer> intList = Arrays.asList(10,5,48,12,10,5,10);
 		Set<Integer> intSet = new HashSet<>();		
 		intList.stream().filter(n -> !intSet.add(n)).collect(Collectors.toSet()).forEach(n -> System.out.println(n));
+		//intList.stream().filter(n -> !intSet.add(n)).forEach(n->System.out.println(n));
 	}
 }
